999a77603651d01df04d1b78c310e953b98a3a5e
zyx898 <55955228+zyx898@users.noreply.github.com>
2019-11-07 23:58:54 -0600

    Update ssense.py

--- ssense.py
+++ ssense.py
@@ -2,136 +2,69 @@
 # -*- coding: utf-8 -*-
 
 
-import requests,threading,random
+import requests
 import json
-import re
 import time
 import datetime
-import random
-from discord_webhook import DiscordEmbed, DiscordWebhook
+from dhooks import Webhook, Embed
+from fake_useragent import UserAgent
+
+
 
 hook_url = '' #webhook for slack
 
 
-sku = '5155781' #Delete 5155781 Input your sku 
+sku = '4184761' #Delete 5155781 Input your sku 
 
-
-
-def getAPI(sku):
-	API = 'https://www.ssense.com/en-us/men/product/zyx/zyx/'+sku+'.json'
-	return API
-
-USER_AGENT =['Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
-		]
+api = 'https://www.ssense.com/en-us/men/product/xymonitor/zyx/'+str(sku)+'.json'
 
 #====================================================================================================================================================================================================#
 #--------------------------------------------------------------------- Time -----------------------------------------------------------------------------------------------------------------------#
 #====================================================================================================================================================================================================#
 
-def gettime():
-	now = str(datetime.datetime.now())
-	
-	now = now.split(' ')[1]
-	
-	threadname = threading.currentThread().getName()
-	
-	threadname = str(threadname).replace('Thread', 'Task')
-	
-	now = '[' + str(now) + ']' + '[' + str(threadname) + ']'
+def random_agent():
+    ua = UserAgent()
+    headers = {"User-Agent": ua.random}
 
-	return now
+    return headers
 
-def message_post(itme_name,link,sizelist,price,item_image):
-	webhook = DiscordWebhook(url=hook_url,content='')
-	embed = DiscordEmbed(title=itme_name,url=link,color=0x00fea9)
-	embed.add_embed_field(name='Price', value=price)
-	embed.add_embed_field(name='Stock', value="\n".join(sizelist))
-	embed.set_thumbnail(url=str(item_image+'?wid=300&hei=300&fmt=png-alpha'))
-	embed.set_footer(text='Ssense Monitor by zyx', icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg')
-	embed.set_timestamp()
-	webhook.add_embed(embed)
-	webhook.execute()
+def post_message(product_link,product_image,product_name,product_price,instock_list):
+    hook = Webhook(hook_url)
+    embed = Embed(description='',color=0x5CDBF0,timestamp='now')
+    embed.set_title(title=product_name,url=product_link)
+    embed.add_field(name='Price',value=product_price)
+    embed.add_field(name='Stock',value='\n'.join(instock_list))
+    embed.set_thumbnail(url=product_image)
+    embed.set_footer(text='Ssense Monitor by zyx', icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg')
+    hook.send(embed=embed)
 
+s = requests.Session()
 
+def task():
+    old_list  = []
+    while True:
+        product_raw_json = s.get(api,headers=random_agent())
+        product_json = json.loads(product_raw_json.text)
+        product_sizes = product_json['product']['sizes']
+        instock_list = []
+        for i in product_sizes:
+            if i['inStock'] == True:
+                instock_list.append(i['number'])
 
-def main(sku):
-	LastList = []
-	while True:
-		API = getAPI(sku)
-		try:
+        difference = [c for c in instock_list if c not in old_list]
 
-			user_agent = random.choice(USER_AGENT)
-			
-			headers = {
-			'User-Agent': user_agent
-			}
+        if difference:
+            product_sku = product_json['product']['sku']
+            product_name = product_json['product']['name']
+            product_id = product_json['product']['id']
+            product_price = product_json['product']['price']['regular']
+            product_link = 'https://www.ssense.com/en-us/men/product/zyx/zyx/' + str(product_id)
+            product_image = 'https://img.ssensemedia.com/images/'+str(product_sku)+'_1/zyx.jpg'
 
-			response = requests.get(API,headers=headers)
-			print(response)
-
-			
-			page = json.loads(response.text)
-						
-			sizes = page['product']['sizes']
-			
-			sizelist = []
-			
-			sizes_sku =[]
-			
-			for i in range(len(sizes)):
-				if sizes[i]['inStock'] == True:
-				
-					sizelist.append(sizes[i]['number'])
-				
-					sizes_sku.append(sizes[i]['sku'])
-				else:
-					pass
-		except:
-				print(gettime() + '[ERROR] --> Unable to Get stock or Get to the api')
-
-		try:
-			intersection = list(set(sizelist).intersection(set(LastList)))
-			
-			difference = list(set(sizelist).difference(set(LastList)))
-
-			if difference:
-				final_list = intersection + difference
-				
-				final_list.sort()
-				
-				LastList = final_list
-				
-				print(gettime() + '[Changes] --> TRUE')
-			
-			else:
-				print(gettime() + '[RESTOCK] --> FALSE')
-				
-				final_list = []
-		
-		except:
-				final_list=[]
-				
-				print(gettime() + '[ERROR] --> Error Compare Sizelis')
-				pass
-
-		if final_list:
-			try:
-				item_id = page['product']['id']
-				
-				itme_name = page['product']['name']
-				
-				sku_1 = page['product']['sku']
-				
-				price = page['product']['price']['regular']
-				
-				link = 'https://www.ssense.com/en-us/men/product/zyx/zyx/' + str(item_id)
-				
-				item_image = 'https://img.ssensemedia.com/images/'+str(sku_1)+'_1/zyx.jpg'
-			
-			except:
-				print(gettime() + '[ERROR] --> Error Getting Fileds or value')
-
-			message_post(itme_name,link,sizelist,price,item_image)
-
-		time.sleep(3)
-main(sku)
+            post_message(product_link,product_image,product_name,product_price,instock_list)
+            print(str(datetime.datetime.now())+" Monitoring---------[ Ssense "+ str(sku) +" ]------Restock[True]")
+            old_list = instock_list
+        else:
+            print(str(datetime.datetime.now())+" Monitoring---------[ Ssense "+ str(sku) +" ]------Restock[False]")
+        time.sleep(3)
+task()
